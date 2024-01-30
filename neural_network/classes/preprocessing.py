31cd2063fcd98003e1cd2230265e3878a1443046
Giovanni Criscione <giovanni2001rg@gmail.com>
2024-01-31 00:03:42 +0100

    add standardization and normalization

--- neural_network/classes/preprocessing.py
+++ neural_network/classes/preprocessing.py
@@ -4,46 +4,63 @@ class Preprocessing:
 
     def __init__(self, name: str):
         self.name = name
-        self.encoder=self.__get(name)
-        
+        self.encoder = self.__get(name)
         
     def __get(self, name: str):
-        # if name == 'standardization':
-        #     return self.__standardization
-        # elif name == 'min_max':
-        #     return self.__min_max
+        # Return the appropriate preprocessing function based on the name
+        if name == 'standardization':
+            return self.__standardization
+        elif name == 'normalization':
+            return self.__normalization
         if name == 'one_hot_encode':
             return self.__one_hot_encode
         elif name == 'none':
             return self.__none
         else:
+            # If an invalid name is provided, raise an error
             raise ValueError('Invalid preprocessing function')
         
-        
+    # Returns the matrix as is (no preprocessing)
+    def __none(self, matrix):
+        return matrix
     
-    def __one_hot_encode(self,matrix):
-        # Definizione del numero di valori unici per ogni colonna
+    # Define the number of unique values for each column (hardcoded for simplicity)
+    def __one_hot_encode(self, matrix):
         unique_values = [3, 3, 2, 3, 4, 2]
 
-        # Calcolo del numero totale di colonne one-hot
+        # Calculate the total number of one-hot columns
         total_one_hot_columns = sum(unique_values)
 
-        # Inizializzazione della matrice one-hot encoded
+        # Initialize the one-hot encoded matrix
         encoded_matrix = np.zeros((matrix.shape[0], total_one_hot_columns))
 
-        # Posizione di inizio per la prima colonna one-hot
+        # Start index for the first one-hot column
         start_index = 0
 
+        # Iterate over each column to create one-hot encoded vectors
         for col_index, n_values in enumerate(unique_values):
             for row_index in range(matrix.shape[0]):
-                # Calcolo dell'indice all'interno della colonna one-hot
+                # Calculate the index within the one-hot column
                 one_hot_index = start_index + matrix[row_index, col_index] - 1
                 encoded_matrix[row_index, one_hot_index] = 1
 
-            # Aggiornamento dell'indice di partenza per la prossima colonna one-hot
+            # Update the start index for the next one-hot column
             start_index += n_values
 
         return encoded_matrix
     
-    def __none(self,matrix):
-        return matrix
+    # Calculate the mean and standard deviation for each column
+    def __standardization(self, matrix):
+        mean = np.mean(matrix, axis=0)
+        std = np.std(matrix, axis=0)
+        # Standardize the matrix
+        standardized_matrix = (matrix - mean) / std
+        return standardized_matrix
+    
+    # Find the minimum and maximum values for each column
+    def __normalization(self, matrix):
+        min_val = np.min(matrix, axis=0)
+        max_val = np.max(matrix, axis=0)
+        # Normalize the matrix
+        normalized_matrix = (matrix - min_val) / (max_val - min_val)
+        return normalized_matrix
--- neural_network/excutor.py
+++ neural_network/excutor.py
@@ -2,16 +2,18 @@ import json
 import pandas as pd
 from classes import Preprocessing
 from neural_network import NeuralNetwork
-
+from dataset import simple_splitter
 
 PATH_CONFIG = "neural_network\\configuration\\neural_network_config.json"
-
 PATH_TRAIN = "neural_network\\dataset\\data_train_val\\training_set.csv"
 PATH_VALIDATION = "neural_network\\dataset\data_train_val\\validation_set.csv"
 PATH_RETRAIN = "neural_network\\dataset\data_train_val\\retrain_set.csv"
 PATH_TEST = "neural_network\\dataset\data_train_val\\test_set.csv"
 
 
+
+
+# read neural network configuration
 def read_neural_network_config():
     """
     Legge un file di configurazione JSON per una rete neurale e salva i dati in un oggetto Python.
@@ -24,78 +26,84 @@ def read_neural_network_config():
     return config
 
 
-def read_dataset(path,encoder_name):
+# read dataset about monks
+def read_dataset(path, encoder_name):
     df = pd.read_csv(path, sep=" ", header=None)
     df.drop(columns=[df.columns[-1]], inplace=True)
     
-    x = df.iloc[:, 2:8].values # tutte le colonne tranne la prima
+    # get all columns except the first (input)
+    x = df.iloc[:, 2:8].values
     
-    y = df.iloc[:, 1].values   # la prima colonna
+    # get the first column (output)
+    y = df.iloc[:, 1].values
     y=y.reshape(-1,1)
+
+    # preprocessing of input
     encoder=Preprocessing(encoder_name)
     x=encoder.encoder(x)
-    
     return x, y
 
 
-def read_dataset_cup(path,encoder_name='none'):
+# read dataset about CUP
+def read_dataset_cup(path, encoder_name):
     df = pd.read_csv(path, sep=",", header=None)
-    #df.drop(columns=[df.columns[-1]], inplace=True)
     
-    x = df.iloc[:, 1:11].values # tutte le colonne tranne la prima
+    # get input
+    x = df.iloc[:, 1:11].values
     
-    y = df.iloc[:, 11:].values # gli ultimi 3 valori
-    #y=y.reshape(-1,1)
+    # get output
+    y = df.iloc[:, 11:].values
+    
+    # preprocessing of input
     encoder=Preprocessing(encoder_name)
     x=encoder.encoder(x)
-    
     return x, y
 
+# create and execute neural network
+def create_neural_network(retrain, test, training_set_size, isCup, name_monks):
+    # read file of configuration
+    config  = read_neural_network_config()
+    encoder_name = config['preprocessing']['name']
 
-def execute(nn, config, test=False):
-    encoder_name=config['preprocessing']['name']
-    #-----------------DATASET-----------------
-    x_train,y_train = read_dataset(PATH_TRAIN,encoder_name)
-    x_validaiton,y_validation = read_dataset(PATH_VALIDATION,encoder_name)
-    x_retrain,y_retrain = read_dataset(PATH_RETRAIN,encoder_name)
-    x_test,y_test = read_dataset(PATH_TEST,encoder_name)
-    
-    #-----------------TRAIN E VALIDATION-----------------    
-    nn.train(x_train, y_train,x_validaiton,y_validation, retrain=False)
-    
+    # create neural network
+    nn = NeuralNetwork(config)
+
+    # create from dataset the training set, validation set, test set and retrain set
+    simple_splitter(training_set_size, isCup, name_monks)
+
+    # set correct function to read dataset
+    if isCup:
+        read = read_dataset_cup
+    else:
+        read = read_dataset
+
+    # get dataset
+    x_train, y_train = read(PATH_TRAIN, encoder_name)
+    x_validation, y_validation = read(PATH_VALIDATION, encoder_name)
+    x_retrain, y_retrain = read(PATH_RETRAIN, encoder_name)
+    x_test, y_test = read(PATH_TEST, encoder_name)
+
+    #-----------------TRAIN E VALIDATION-----------------
+    nn.train(x_train, y_train, x_validation, y_validation, retrain=False)
+
     #-----------------RETRAIN-----------------
-    #nn.train(x_retrain,y_retrain,x_validaiton,y_validation, retrain=True)
+    if retrain:
+        print("Retraining...")
+        nn.train(x_retrain, y_retrain, x_validation, y_validation, retrain=True)
     
     #-----------------TEST-----------------
     if test:
+        print("Testing...")
         nn.test(x_test,y_test)
 
 
-def execute_cup(nn, config, test=False):
-    encoder_name=config['preprocessing']['name']
-    #-----------------DATASET-----------------
-    x_train,y_train = read_dataset_cup(PATH_TRAIN,encoder_name)
-    x_validaiton,y_validation = read_dataset_cup(PATH_VALIDATION,encoder_name)
-    x_retrain,y_retrain = read_dataset_cup(PATH_RETRAIN,encoder_name)
-    x_test,y_test = read_dataset_cup(PATH_TEST,encoder_name)
-    
-    #-----------------TRAIN E VALIDATION-----------------    
-    nn.train(x_train, y_train,x_validaiton,y_validation, retrain=False)
-    
-    #-----------------RETRAIN-----------------
-    #nn.train(x_retrain,y_retrain,x_validaiton,y_validation, retrain=True)
-    
-    #-----------------TEST-----------------
-    if test:
-        nn.predict(x_test)
 
-    
+# define parameters
+retrain=False
+test=False
+training_set_size=0.8
+isCup=True
+name_monks="monks-1"
 
-
-config = read_neural_network_config()
-nn = NeuralNetwork(config)
-#execute(nn, config, test=True)
-execute_cup(nn, config)
-
-
-
+# create and execute neural network
+create_neural_network(retrain, test, training_set_size, isCup, name_monks)
